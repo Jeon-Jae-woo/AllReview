b8fc2de021d42bb85ac0e062f6c83f0b229fd210
jeongho <jh77c@naver.com>
2021-07-07 21:37:35 +0900

    이전까지 작업 내용

--- src/com/online/controller/onlineController.java
+++ src/com/online/controller/onlineController.java
@@ -1,41 +1,178 @@
 package com.online.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class onlineController
- */
+import com.online.biz.onlineBiz;
+import com.online.biz.onlineBizImpl;
+import com.online.dto.onlineDto;
+
 @WebServlet("/onlineController")
 public class onlineController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    
     public onlineController() {
         super();
-        // TODO Auto-generated constructor stub
+       
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		//커맨드로 넘어오는 값, 콘솔창 커맨드값
+		String command = request.getParameter("command");
+		System.out.println("["+command+"]");
+		
+		onlineBiz biz = new onlineBizImpl();
+		
+		//전체조회
+		if(command.equals("list")) {
+			List<onlineDto> list = biz.selectAll();
+			
+			request.setAttribute("list", list);
+			dispatch("board_main.jsp",request,response);
+			
+			
+		//카테고리 게시판 별 조회
+		}else if(command.equals("fashion")) {
+			List<onlineDto> fashion = biz.selectFashion();
+			
+			request.setAttribute("fashion", fashion);
+			dispatch("board_fashion.jsp",request,response);
+			
+			
+		}else if(command.equals("beauty")) {
+			List<onlineDto> beauty= biz.selectBeauty();
+			
+			request.setAttribute("beauty", beauty);
+			dispatch("board_beauty.jsp",request,response);
+			
+			
+		}else if(command.equals("food")) {
+			List<onlineDto> food = biz.selectFood();
+			
+			request.setAttribute("food", food);
+			dispatch("board_food.jsp",request,response);
+			
+		}else if(command.equals("home")) {
+			List<onlineDto> home = biz.selectHome();
+			
+			request.setAttribute("home", home);
+			dispatch("board_home.jsp",request,response);
+			
+		}else if(command.equals("digital")) {
+			List<onlineDto> digital = biz.selectDigital();
+			
+			request.setAttribute("digital", digital);
+			dispatch("board_digital.jsp",request,response);
+			
+		}else if(command.equals("appliances")) {
+			List<onlineDto> appliances = biz.selectAppliances();
+				
+			request.setAttribute("appliances", appliances);
+			dispatch("board_appliances.jsp",request,response);
+			
+		}else if(command.equals("childcare")) {
+			List<onlineDto> childcare = biz.selectChildcare();
+				
+			request.setAttribute("childcare", childcare);
+			dispatch("board_childcare.jsp",request,response);
+			
+		}else if(command.equals("medical")) {
+			List<onlineDto> medical = biz.selectMedical();
+				
+			request.setAttribute("medical", medical);
+			dispatch("board_medical.jsp",request,response);
+			
+		}else if(command.equals("hobby")) {
+			List<onlineDto> hobby = biz.selectHobby();
+				
+			request.setAttribute("hobby", hobby);
+			dispatch("board_hobby.jsp",request,response);
+			
+		}else if(command.equals("sports")) {
+			List<onlineDto> sports = biz.selectSports();
+				
+			request.setAttribute("sports", sports);
+			dispatch("board_sports.jsp",request,response);
+			
+		}else if(command.equals("pet")) {
+			List<onlineDto> pet = biz.selectPet();
+				
+			request.setAttribute("pet", pet);
+			dispatch("board_pet.jsp",request,response);
+			
+			
+		}else if(command.equals("car")) {
+			List<onlineDto> car = biz.selectCar();
+					
+			request.setAttribute("car", car);
+			dispatch("board_car.jsp",request,response);
+				
+			
+		//상세조회
+		} else if(command.equals("detail")) {
+			int online_board_id = Integer.parseInt(request.getParameter("online_board_id"));
+			
+			onlineDto dto = biz.selectOne(online_board_id);
+			
+			request.setAttribute("dto", dto);
+			dispatch("boarddetail.jsp",request,response);
+			
+			
+			
+		} else if(command.equals("writeform")) {
+			response.sendRedirect("boardwrite.jsp");
+		}else if(command.equals("boardwrite")) {
+			
+			
+		}else if(command.equals("updateform")) {
+			
+		}else if(command.equals("boardupdate")) {
+			
+		}else if(command.equals("delete")) {
+			int online_board_id = Integer.parseInt(request.getParameter("online_board_id"));
+			
+			boolean res = biz.delete(online_board_id);
+			
+			if(res) {
+				jsResponse("글 삭제 성공", "controller.do?command=list", response);
+			}else {
+				jsResponse("글 삭제 실패", "controller.do?command=detail&online_board_id="+online_board_id,response);
+			}
+			
+			
+		}
+		
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+				doGet(request, response);
 	}
-
+	private void jsResponse(String msg, String url, HttpServletResponse response) throws IOException {
+		String s = "<script type='text/javascript'>"
+				+ "alert('"+msg+"');"
+				+ "location.href='"+url+"';"
+				+ "</script>";
+				
+		PrintWriter out = response.getWriter();
+		out.print(s);
+	}
+	private void dispatch(String url, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatch = request.getRequestDispatcher(url); //메소드 실행시 넘겨줄 url
+		dispatch.forward(request, response);
+	}
 }
--- src/com/online/dao/onlineDao.java
+++ src/com/online/dao/onlineDao.java
@@ -1,5 +1,49 @@
 package com.online.dao;
 
-public class onlineDao {
+import java.sql.Connection;
+import java.util.List;
 
+import com.online.dto.onlineDto;
+
+
+public interface onlineDao {
+
+	String selectAllSql = " SELECT * FROM ONLINE_BOARD ORDER BY ONLINE_BOARD_ID DESC ";
+	String selectOneSql =" SELECT * FROM ONLINE_BOARD WHERE ONLINE_BOARD_ID=? ";
+	String insertSql = "";
+	String updateSql ="";
+	String deleteSql =" DELETE FROM ONLINE_BOARD WHERE ONLINE_BOARD_ID=? ";
+	
+	public List<onlineDto> selectAll(Connection con); 
+	public onlineDto selectOne(Connection con, int online_board_id);
+	public boolean insert(Connection con, onlineDto dto);
+	public boolean update(Connection con, onlineDto dto);
+	public boolean delete(Connection con, int online_board_id);
+	
+	/*카테고리 select*/
+	public List<onlineDto> selectFashion(Connection con);
+	public List<onlineDto> selectBeauty(Connection con);
+	public List<onlineDto> selectFood(Connection con);
+	public List<onlineDto> selectHome(Connection con);
+	public List<onlineDto> selectDigital(Connection con);
+	public List<onlineDto> selectAppliances(Connection con);
+	public List<onlineDto> selectChildcare(Connection con);
+	public List<onlineDto> selectMedical(Connection con);
+	public List<onlineDto> selectHobby(Connection con);
+	public List<onlineDto> selectSports(Connection con);
+	public List<onlineDto> selectPet(Connection con);
+	public List<onlineDto> selectCar(Connection con);
+	
+	String selectfashionSql = " SELECT * FROM ONLINE_BOARD WHERE CATEGORY_ID = '1' ORDER BY ONLINE_BOARD_ID DESC ";
+	String selectbeautySql = " SELECT * FROM ONLINE_BOARD WHERE CATEGORY_ID = '2' ORDER BY ONLINE_BOARD_ID DESC ";
+	String selectfoodSql = " SELECT * FROM ONLINE_BOARD WHERE CATEGORY_ID = '3' ORDER BY ONLINE_BOARD_ID DESC ";
+	String selecthomeSql = " SELECT * FROM ONLINE_BOARD WHERE CATEGORY_ID = '4' ORDER BY ONLINE_BOARD_ID DESC ";
+	String selectdigitalSql = " SELECT * FROM ONLINE_BOARD WHERE CATEGORY_ID = '5' ORDER BY ONLINE_BOARD_ID DESC ";
+	String selectappliancesSql = " SELECT * FROM ONLINE_BOARD WHERE CATEGORY_ID = '6' ORDER BY ONLINE_BOARD_ID DESC ";
+	String selectchildcareSql = " SELECT * FROM ONLINE_BOARD WHERE CATEGORY_ID = '7' ORDER BY ONLINE_BOARD_ID DESC ";
+	String selectmedicalSql = " SELECT * FROM ONLINE_BOARD WHERE CATEGORY_ID = '8' ORDER BY ONLINE_BOARD_ID DESC ";
+	String selecthobbySql = " SELECT * FROM ONLINE_BOARD WHERE CATEGORY_ID = '9' ORDER BY ONLINE_BOARD_ID DESC ";
+	String selectsportsSql= " SELECT * FROM ONLINE_BOARD WHERE CATEGORY_ID = '10' ORDER BY ONLINE_BOARD_ID DESC ";
+	String selectpetSql = " SELECT * FROM ONLINE_BOARD WHERE CATEGORY_ID = '11' ORDER BY ONLINE_BOARD_ID DESC ";
+	String selectcarSql = " SELECT * FROM ONLINE_BOARD WHERE CATEGORY_ID = '12' ORDER BY ONLINE_BOARD_ID DESC ";
 }
